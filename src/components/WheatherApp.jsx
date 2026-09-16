7151e0f9c90a24f6938f389bf588ded2cddb4fba
ISABELLE VITORIA DE MATOS <isabelle.vmatos@senacsp.edu.br>
2026-09-15 21:35:32 -0300

    feat: control weather location input

--- src/components/WheatherApp.jsx
+++ src/components/WheatherApp.jsx
@@ -1,6 +1,20 @@
 import sunny from '../assets/images/sunny.png'
 
+import { useState } from 'react'
+
+
 const WheatherApp = () => {
+    const [location, setLocation] = useState('')
+
+    const handleInputChanges = (e) => {
+        setLocation(e.target.value)
+      }
+
+
+
+
+console.log(location)
+
   return (
     <div className="container">
       <div className="weather-app">
@@ -11,10 +25,12 @@ const WheatherApp = () => {
           </div>
 
           <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Enter Location"
-            />
+          <input
+            type="text"
+            placeholder="Enter Location"
+            value={location}
+            onChange={handleInputChanges}
+        />  
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
         </div>
